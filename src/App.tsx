bc9c6ae193ea0077125c5faf476cd06a03ae42b8
Terryb <terrydagbartels@gmail.com>
2021-11-29 06:58:17 +0000

    Boilerplate removed, container and font configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,21 @@
 import * as React from "react"
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
+  extendTheme,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import Fonts from './Fonts'
+import DashboardContainer from "./containers/DashboardContainer"
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Airbnb Cereal App",
+    body: "Airbnb Cereal App"
+  }
+})
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+    <Fonts />
+    <DashboardContainer />
   </ChakraProvider>
 )
